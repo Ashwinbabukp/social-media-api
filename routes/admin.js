828eabb18e361b06dd2c21bd111d3a9f99e5bc5d
Ashwinbabukp <kpashwin045@gmail.com>
2021-12-12 00:27:44 +0530

    Social Media API add post

---
+++ routes/admin.js
@@ -0,0 +1,42 @@
+var express = require('express');
+const { request } = require('../app');
+const productHelpers = require('../helpers/product-helpers');
+var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('admin/view-products', { admin: true, products })
+
+  })
+  
+  
+});
+router.get('/add-product', function (req, res) {
+  res.render('admin/add-product')
+
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image);
+
+
+  productHelpers.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    console.log(id);
+    image.mv('./public/images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+        res.redirect('/')
+      }else{
+        console.log(err);
+      }
+    })
+    
+  });
+
+})
+
+module.exports = router;
